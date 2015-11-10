f97994040cb0eb72f52e00078a1ad6a335430b1f
Yannis Guedel <me@yannisguedel.ch>
2015-11-10 08:55:48 +0100

    implemented incremental update

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -1,12 +1,15 @@
 import Freezer from 'freezer-js'
 import oevents from '../data/OEventsService'
 import localStorage from '../data/LocalStorage'
+require('array.prototype.find');
+var _ = require('lodash');
 
 let store = new Freezer({events: []});
 
 export default store;
 
 async function loadEvents() {
+
     var items = JSON.parse(await localStorage.get("events"));
     store.get().events.set(items);
 
@@ -15,6 +18,21 @@ async function loadEvents() {
         lastUpdate = 0;
     }
     var events = await oevents.getAll(lastUpdate);
+    console.log(events)
+    for(var i in items) {
+        var item = items[i];
+        var ev = events.find((e) => item.id == e.id);
+        if(ev) {
+            items[i] = ev;
+        }
+        else {
+
+            items.push(ev)
+        }
+    }
+    var events = _.sortBy(events, 'date');
+    console.log(events)
+
     await localStorage.set("events", JSON.stringify(events));
     store.get().events.set(events);
     await localStorage.set("lastEventsUpdate", new Date().getTime().toString());
